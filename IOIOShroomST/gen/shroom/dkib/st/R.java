3d9444461b3e657f40c71ac5626b22736445f850
deanzaka <deanzaka.h@gmail.com>
2014-11-17 05:51:53 +0700

    first activity already working, but the clock still on IOIO loop, gonna send it to a new thread

--- IOIOShroomST/gen/shroom/dkib/st/R.java
+++ IOIOShroomST/gen/shroom/dkib/st/R.java
@@ -15,18 +15,19 @@ public final class R {
         public static final int ioio_down=0x7f020001;
     }
     public static final class id {
-        public static final int AutoMode=0x7f06000b;
-        public static final int CelLabel=0x7f060006;
-        public static final int Lamp=0x7f06000a;
-        public static final int LightLabel=0x7f060001;
-        public static final int LuxLabel=0x7f060003;
-        public static final int LuxValue=0x7f060002;
-        public static final int TempLabel=0x7f060004;
-        public static final int TempValue=0x7f060005;
-        public static final int Terminal1=0x7f060007;
-        public static final int Terminal2=0x7f060008;
-        public static final int Terminal3=0x7f060009;
-        public static final int TestTime=0x7f060000;
+        public static final int AutoMode=0x7f06000c;
+        public static final int CelLabel=0x7f060007;
+        public static final int DisplayDate=0x7f060001;
+        public static final int DisplayTime=0x7f060000;
+        public static final int Lamp=0x7f06000b;
+        public static final int LightLabel=0x7f060002;
+        public static final int LuxLabel=0x7f060004;
+        public static final int LuxValue=0x7f060003;
+        public static final int TempLabel=0x7f060005;
+        public static final int TempValue=0x7f060006;
+        public static final int Terminal1=0x7f060008;
+        public static final int Terminal2=0x7f060009;
+        public static final int Terminal3=0x7f06000a;
     }
     public static final class layout {
         public static final int main=0x7f030000;
